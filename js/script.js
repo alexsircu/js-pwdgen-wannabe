d46c02361f45c79457e69070ba0bf7fb8678c57b
Alex Sircu <alex.sricu@gmail.com>
2020-11-10 16:03:37 +0100

    Fatto anche il test con console.log in maniera corretta

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 var name = prompt("Qual è il tuo nome?");
-console.log("name");
+console.log(name);
 
 var surname = prompt("Qual è il tuo cognome?");
+console.log(surname);
 
 var color = prompt("Qual è il tuo colore preferito?");
+console.log(color);
 
 var number = 19;
+console.log(number);
 
 document.getElementById("client-name").innerHTML = name;
 
